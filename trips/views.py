5a18d132d68562170b065315429ec82610e113d6
John Sterling <jsterling@cashstar.com>
2016-07-03 11:47:42 -0400

    Issue #2: start to add the views to be able to flush out location model.

--- trips/views.py
+++ trips/views.py
@@ -1,13 +1,19 @@
+from django.shortcuts import get_object_or_404,render
 from django.http import HttpResponse
-
+from trips.models import Trip
+from django.template import loader
 
 # Trip access
 def index(request):
     return list_trips(request)
 
 def trip_detail(request, trip_id):
-    return HttpResponse('You found trip %s' % trip_id)
+    trip = get_object_or_404(Trip, pk=trip_id)
+    return render(request, 'trips/detail.html', {'trip': trip})
 
 def list_trips(request):
-    return HttpResponse('All Trips')
-
+    trip_list = Trip.objects.order_by('-trip_date')[:5]
+    context = {
+        'trip_list': trip_list,
+    }
+    return render(request, 'trips/index.html', context)
